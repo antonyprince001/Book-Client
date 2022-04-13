9d1564db714cf90b1f6a2778028767504d0ec338
Antony Prince J <antony.prince@thoughtworks.com>
2022-04-13 20:50:20 +0530

    [Antony] Refactor the Navbar Component

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import Navbar from './Components/Navbar';
 
 describe('APP Component', ()=>{
 
+  let container;
+
+  beforeEach(() => (container = shallow(<App />)))
+
   it('Should render App Component',()=>{
-    const Container = shallow(<App />);
-    expect(Container.find('div').length).toEqual(1);
+    expect(container.find('div').length).toEqual(1);
   });
 
   it('Should render Navbar Component',()=>{
-    const Container = shallow(<App />);
-    expect(Container.find('Navbar').length).toEqual(1);
+    expect(container.containsMatchingElement(<Navbar />)).toEqual(true)
   });
 
 })
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,7 @@ const Navbar = () => {
     return (
         <nav class="navbar navbar-light bg-light">
             <div class="container-fluid">
-                <span class="navbar-brand mb-0 h1">Navbar</span>
+                <span class="navbar-brand mb-0 h1">Book Inventory</span>
             </div>
         </nav>
     );
