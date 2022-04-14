0624a51fc06a5159c584d776622b6f6ad5ead069
Antony Prince J <antony.prince@thoughtworks.com>
2022-04-14 23:20:40 +0530

    [Antony] Add logic to add book using Book API

--- src/Routes/BookCreate.js
+++ src/Routes/BookCreate.js
@@ -1,9 +1,46 @@
+import React, { useContext } from 'react';
+import BookAPI from '../APIs/BookAPI';
 import BookForm from '../Components/BookForm';
+import { BookContext } from '../Context/BookContext';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const BookCreate = () => {
+
+    const { addBook } = useContext(BookContext);
+    const notify = (msg) => toast(msg);
+
+    const book = {
+        name : "",
+        author : "",
+        price : 0,
+        rating : 0
+    };
+
+    const createBook = async (name,author,price,rating)=>{
+        try{
+        const response =  await BookAPI.post("/book", {
+            name,
+            author,
+            price,
+            rating
+          }
+          );
+          console.log(response);
+          addBook(response.data);
+        }catch(err){}
+    } 
+
     return (
         <div>
-            <BookForm />
+            <center>
+            <br /><br />
+            <ToastContainer />
+            <h3>Add Book</h3>
+            <br />
+            <BookForm book={book} type="Add Book" handleSubmit={createBook} notify={notify}/>
+            </center>
         </div>
     );
 }
